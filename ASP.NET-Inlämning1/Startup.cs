351ce5527a616dcdebcec5001d00cd100f36a9d3
Alan Weik <alaan.m@hotmail.com>
2021-03-22 18:26:40 +0100

    Registered context as service in Startup.

--- ASP.NET-Inlämning1/Startup.cs
+++ ASP.NET-Inlämning1/Startup.cs
@@ -27,12 +27,9 @@ namespace ASP.NET_Inlämning1
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDatabaseDeveloperPageExceptionFilter();
-            services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddDbContext<EventDbContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DbContext")));
+        
             services.AddRazorPages();
         }
 
